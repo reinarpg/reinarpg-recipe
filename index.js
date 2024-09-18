4fd472df2912ba7542f106544e1811e3de6b3d5f
rafucuk <raufsen11@gmail.com>
2024-09-18 15:12:05 +0000

    Push

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function loader (registryOrVersion) {
-  const registry = typeof registryOrVersion === 'string' ? require('prismarine-registry')(registryOrVersion) : registryOrVersion
+  const registry = typeof registryOrVersion === 'string' ? require('reinarpg-registry')(registryOrVersion) : registryOrVersion
   return {
     Recipe: require('./lib/recipe')(registry),
     RecipeItem: require('./lib/recipe_item')
